5828a2686ce662868cb2fe00ac8ef691e63eced6
Macbook Pro Retina <hello@Macbooks-MacBook-Pro.local>
2018-12-28 02:13:22 +0600

    Add intro section in the homepage also Employee Login Page Added

--- src/app/client/authentication/authentication.component.ts
+++ src/app/client/authentication/authentication.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NbSpinnerService} from "@nebular/theme";
 
 @Component({
-  selector: 'ngx-authentication',
-  templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.scss']
+    selector: 'ngx-authentication',
+    templateUrl: './authentication.component.html',
+    styleUrls: ['./authentication.component.scss']
 })
 export class AuthenticationComponent implements OnInit {
 
-  constructor() { }
+    constructor(private spinnerService: NbSpinnerService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.spinnerService.load();
+    }
 
 }
