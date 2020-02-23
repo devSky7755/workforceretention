4a41b8f4997e52e70665e36a43da7978de8bb4e2
Sina Karimi <creamycode@gmail.com>
2020-02-23 16:07:12 +1100

    Rename flexible options to program options

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AuthenticationComponent } from "./authentication/authentication.component";
 import { ProductComponent } from "./product/product.component";
 import { ContactComponent } from "./contact/contact.component";
 import { BlogComponent } from "./blog/blog.component";
-import { BlogDetailComponent } from './blog/blog-detail/blog-detail.component';
+import { BlogDetailComponent } from "./blog/blog-detail/blog-detail.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { QuestionsComponent } from "./questions/questions.component";
 import { ProfileComponent } from "./profile/profile.component";
@@ -15,34 +15,33 @@ import { ReportsComponent } from "./reports/reports.component";
 import { ManagerReportPdfComponent } from "./manager-report-pdf/manager-report-pdf.component";
 import { FlexibleOptionsComponent } from "./flexible-options/flexible-options.component";
 import { AboutComponent } from "./about/about.component";
-import { RequestAQuoteComponent } from './request-a-quote/request-a-quote.component'
+import { RequestAQuoteComponent } from "./request-a-quote/request-a-quote.component";
 import { PrivacyComponent } from "./privacy/privacy.component";
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'login', component: AuthenticationComponent },
-    { path: 'blogs', redirectTo: 'blogs/0' },
-    { path: 'blogs/:pageNum', component: BlogComponent },
-    { path: 'blog/:id', component: BlogDetailComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: 'product', component: ProductComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'report', component: ReportsComponent },
-    { path: 'employee-survey', component: EmployeeSurveyDownloadComponent },
-    { path: 'questions/:id', component: QuestionsComponent },
-    { path: 'manager-report', component: ManagerReportComponent },
-    { path: 'manager-report-pdf', component: ManagerReportPdfComponent },
-    { path: 'flexible-options', component: FlexibleOptionsComponent },
-    { path: 'about-us', component: AboutComponent },
-    { path: 'privacy', component: PrivacyComponent },
-    { path: 'request-a-quote', component: RequestAQuoteComponent },
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "login", component: AuthenticationComponent },
+  { path: "blogs", redirectTo: "blogs/0" },
+  { path: "blogs/:pageNum", component: BlogComponent },
+  { path: "blog/:id", component: BlogDetailComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "product", component: ProductComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "report", component: ReportsComponent },
+  { path: "employee-survey", component: EmployeeSurveyDownloadComponent },
+  { path: "questions/:id", component: QuestionsComponent },
+  { path: "manager-report", component: ManagerReportComponent },
+  { path: "manager-report-pdf", component: ManagerReportPdfComponent },
+  { path: "program-options", component: FlexibleOptionsComponent },
+  { path: "about-us", component: AboutComponent },
+  { path: "privacy", component: PrivacyComponent },
+  { path: "request-a-quote", component: RequestAQuoteComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class ClientRoutingModule {
-}
+export class ClientRoutingModule {}
--- src/app/client/layout/header/header.component.ts
+++ src/app/client/layout/header/header.component.ts
@@ -1,93 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
 import { Router, NavigationEnd } from "@angular/router";
 import { EmployeeService } from "../../../@core/data/employee.service";
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { JwtHelperService } from "@auth0/angular-jwt";
 import { ClientService } from "../../../@core/data/client.service";
-import { environment } from '../../../../environments/environment';
-import 'rxjs/add/operator/filter';
+import { environment } from "../../../../environments/environment";
+import "rxjs/add/operator/filter";
 
 @Component({
-    selector: 'ngx-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: "ngx-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
-    isAuth: boolean = false;
-    authSubscription: Subscription;
-    employee_details;
-    clientImage = null
-    baseUrl = !environment.production ? 'http://localhost:8080' : ''
-    isDefaultLogo = false
-    showPhoneNo = false
-    routerLink = '/client/home'
+  isAuth: boolean = false;
+  authSubscription: Subscription;
+  employee_details;
+  clientImage = null;
+  baseUrl = !environment.production ? "http://localhost:8080" : "";
+  isDefaultLogo = false;
+  showPhoneNo = false;
+  routerLink = "/client/home";
 
-    constructor(private router: Router,
-        private employeeService: EmployeeService,
-        private clientService: ClientService) {
-        router.events.filter((event: any) => event instanceof NavigationEnd).subscribe(event => {
-            let defaultLogoUrl = ['/client/login']
-            if (defaultLogoUrl.includes(event.url)) {
-                this.isDefaultLogo = true
-            }
-            let RAQUrls = ['/flexible-options']
-            if (RAQUrls.includes(event.url)) {
-                this.showPhoneNo = true
-            }
-        });
-    }
-
-    ngOnInit() {
-        // here parse the employee
-        // check the localStorage. if get the user id then set isAuth to true
-        if (localStorage.getItem('employee')) {
-            // parse the employee object and check the expiration of the login. if the login time is expired
-            const employee = JSON.parse(localStorage.getItem('employee'));
-            // then execute login function
-            const helper = new JwtHelperService();
-            // const decodedToken = helper.decodeToken(employee.access_token);
-            // console.log(decodedToken);
-            this.employee_details = helper.decodeToken(employee.access_token);
-            if (helper.isTokenExpired(employee.access_token)) {
-                this.logout();
-            } else {
-                this.isAuth = true;
-                if (this.employee_details && this.employee_details.is_survey == 1) {
-                    this.routerLink = '/client/dashboard'
-                } else if (this.employee_details && this.employee_details.is_report == 1) {
-                    this.routerLink = '/client/manager-report'
-                }
-                this.clientImage = this.baseUrl + '/images/client/' + (this.employeeService.clientImage || '')
-                if (this.employeeService.clientImage === '') {
-                    const decodedToken = helper.decodeToken(employee.access_token);
-                    this.getClient(decodedToken.client);
-                }
-            }
+  constructor(
+    private router: Router,
+    private employeeService: EmployeeService,
+    private clientService: ClientService
+  ) {
+    router.events
+      .filter((event: any) => event instanceof NavigationEnd)
+      .subscribe(event => {
+        let defaultLogoUrl = ["/client/login"];
+        if (defaultLogoUrl.includes(event.url)) {
+          this.isDefaultLogo = true;
         }
-        this.authSubscription = this.employeeService.authChange.subscribe(authStatus => {
-            this.isAuth = authStatus;
-        });
-    }
+        let RAQUrls = ["/program-options"];
+        if (RAQUrls.includes(event.url)) {
+          this.showPhoneNo = true;
+        }
+      });
+  }
 
-    getClient(client_id) {
-        // here get the client and set the image
-        this.clientService.getClient(client_id).subscribe(
-            res => {
-                console.log(res);
-                this.employeeService.clientImage = res.client.image;
-                this.clientImage = this.baseUrl + '/images/client/' + (this.employeeService.clientImage || '')
-            }
-        );
+  ngOnInit() {
+    // here parse the employee
+    // check the localStorage. if get the user id then set isAuth to true
+    if (localStorage.getItem("employee")) {
+      // parse the employee object and check the expiration of the login. if the login time is expired
+      const employee = JSON.parse(localStorage.getItem("employee"));
+      // then execute login function
+      const helper = new JwtHelperService();
+      // const decodedToken = helper.decodeToken(employee.access_token);
+      // console.log(decodedToken);
+      this.employee_details = helper.decodeToken(employee.access_token);
+      if (helper.isTokenExpired(employee.access_token)) {
+        this.logout();
+      } else {
+        this.isAuth = true;
+        if (this.employee_details && this.employee_details.is_survey == 1) {
+          this.routerLink = "/client/dashboard";
+        } else if (this.employee_details && this.employee_details.is_report == 1) {
+          this.routerLink = "/client/manager-report";
+        }
+        this.clientImage = this.baseUrl + "/images/client/" + (this.employeeService.clientImage || "");
+        if (this.employeeService.clientImage === "") {
+          const decodedToken = helper.decodeToken(employee.access_token);
+          this.getClient(decodedToken.client);
+        }
+      }
     }
+    this.authSubscription = this.employeeService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
 
-    logout() {
-        // remove the employee from the localStorage
-        localStorage.removeItem('employee');
-        // employeeService authChange make this false
-        this.employeeService.authChange.next(false);
-        this.employeeService.employee = null;
-        //redirect to login page
-        this.router.navigateByUrl('/client/login');
-    }
+  getClient(client_id) {
+    // here get the client and set the image
+    this.clientService.getClient(client_id).subscribe(res => {
+      console.log(res);
+      this.employeeService.clientImage = res.client.image;
+      this.clientImage = this.baseUrl + "/images/client/" + (this.employeeService.clientImage || "");
+    });
+  }
 
+  logout() {
+    // remove the employee from the localStorage
+    localStorage.removeItem("employee");
+    // employeeService authChange make this false
+    this.employeeService.authChange.next(false);
+    this.employeeService.employee = null;
+    //redirect to login page
+    this.router.navigateByUrl("/client/login");
+  }
 }
